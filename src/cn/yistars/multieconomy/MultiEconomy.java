11771c0a03d8d7e229d5bbef251686ef71690557
冰砚炽 <36477925+Bing-Yanchi@users.noreply.github.com>
2020-05-08 19:55:53 +0800

    update

--- src/cn/yistars/multieconomy/MultiEconomy.java
+++ src/cn/yistars/multieconomy/MultiEconomy.java
@@ -16,5 +16,52 @@ public class MultiEconomy extends JavaPlugin {
 	public static MultiEconomy getInstance() {
 		return instance;
 	}
+	
+    @Override
+    public void onEnable(){
+        instance = this;
+
+        nmsVersion = new NMSVersion();
+        chequeManager = new ChequeManager(this);
+        economyLogger = new EcoLogger(this);
+        metrics = new Metrics(this);
+        updateForwarder = new UpdateForwarder();
+
+        initializeDataStore(getConfig().getString("storage"), true);
+
+        getServer().getPluginManager().registerEvents(new EconomyListener(), this);
+        getCommand("balance").setExecutor(new BalanceCommand());
+        getCommand("baltop").setExecutor(new BalanceTopCommand());
+        getCommand("economy").setExecutor(new EconomyCommand());
+        getCommand("pay").setExecutor(new PayCommand());
+        getCommand("currency").setExecutor(new CurrencyCommand());
+        getCommand("cheque").setExecutor(new ChequeCommand());
+        getCommand("exchange").setExecutor(new ExchangeCommand());
+
+        vaultHandler = new VaultHandler(this);
+        vaultHandler.hook();
+        UtilServer.consoleLog("Vault compatibility enabled.");
+
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", updateForwarder);
+
+        if(isLogging()) {
+            getEconomyLogger().save();
+        }
+
+        doAsync(() -> checkForUpdate());
+    }
+
+    @Override
+    public void onDisable() {
+        disabling = true;
+
+        if(isVault()) getVaultHandler().unhook();
+        if(isLogging()) EconomyLogger.closeLog();
+
+        if (GemsEconomy.getDataStore() != null) {
+            GemsEconomy.getDataStore().close();
+        }
+    }
 
 }
